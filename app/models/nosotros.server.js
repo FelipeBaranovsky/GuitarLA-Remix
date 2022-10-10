838708080965a506503bb46b553d3f6e3ef6281b
FelipeBaranovsky <fbaranovsky@gmail.com>
2022-10-10 18:36:21 -0300

    Fixing

--- app/models/nosotros.server.js
+++ app/models/nosotros.server.js
@@ -1,5 +1,5 @@
 export async function getNosotros() {
-    const respuesta = await fetch(`${process.env.production.API_URL}/nosotro?populate=imagen`)
+    const respuesta = await fetch(`${process.env.API_URL}/nosotro?populate=imagen`)
     const resultado = await respuesta.json()
     return resultado
 }

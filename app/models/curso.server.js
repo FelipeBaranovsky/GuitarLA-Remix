63f1b6e73a8af3dc6749ecacc6f6a93ce4b6d6bd
FelipeBaranovsky <fbaranovsky@gmail.com>
2022-10-10 17:21:07 -0300

    Fixing env

--- app/models/curso.server.js
+++ app/models/curso.server.js
@@ -1,5 +1,5 @@
 export async function getCurso() {
-    const respuesta = await fetch(`${process.env.API_URL}/curso?populate=imagen`)
+    const respuesta = await fetch(`${process.env.production.API_URL}/curso?populate=imagen`)
     const resultado = await respuesta.json()
     return resultado
 }
